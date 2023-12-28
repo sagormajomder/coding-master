de6831ec009946fb2388b5c9ec7cd33054fded45
Susmita <susmitagain85@gmail.com>
2023-12-28 14:49:32 +0600

    solves issues #1: Reconstruct home header design

--- App/Pages/Home.jsx
+++ App/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Colors from '../Common/Colors';
 import Header from '../Components/Home/Header';
 
 export default function Home() {
   return (
     <View style={styles.homeContainer}>
-      <Header />
+      <View style={styles.headerContainer}>
+        <Header />
+      </View>
     </View>
   );
 }
@@ -13,5 +16,11 @@ export default function Home() {
 const styles = StyleSheet.create({
   homeContainer: {
     flex: 1,
+  },
+  headerContainer: {
+    paddingTop: 10,
+    paddingHorizontal: 15,
+    backgroundColor: Colors.PRIMARY,
+    height: 250,
   },
 });
